157f5e07885eb21bb191b351cb1d5cf1cef1bffd
siddsabata <siddharth@sabata.net>
2025-10-01 13:34:10 -0400

    dummy extra features in main.py

--- src/main.py
+++ src/main.py
@@ -109,10 +109,7 @@ def main(cfg: DictConfig):
         train_metrics = TrainAbstractMetricsDiscrete() if cfg.model.type == 'discrete' else TrainAbstractMetrics()
         visualization_tools = NonMolecularVisualization()
 
-        if cfg.model.type == 'discrete' and cfg.model.extra_features is not None:
-            extra_features = ExtraFeatures(cfg.model.extra_features, dataset_info=dataset_infos)
-        else:
-            extra_features = DummyExtraFeatures()
+        extra_features = DummyExtraFeatures()
         domain_features = DummyExtraFeatures()
 
         dataset_infos.compute_input_output_dims(datamodule=datamodule, extra_features=extra_features,
